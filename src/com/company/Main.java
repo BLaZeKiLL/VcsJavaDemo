021f2645d10a3f1df59af3115a7e3a116c261be0
BLaZeKiLL <laldevashish@gmail.com>
2017-12-20 13:02:07 +0530

    sub() added2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class Main {
 
+    static int subtract(int a,int b)
+    {
+        return a-b;
+    }
+
     public static void main(String[] args)
     {
 	    System.out.println("Supp people ??");
